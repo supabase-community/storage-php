fac10f3fe34b0e1331947533a8887f42e9576ecd
Norm <tehnorm@users.noreply.github.com>
2023-02-14 02:25:15 +0000

    Apply fixes from StyleCI

--- examples/StorageFileSamples.php
+++ examples/StorageFileSamples.php
@@ -4,10 +4,10 @@ include '../vendor/autoload.php';
 
 use Supabase\Storage\StorageFile;
 
-$authHeader = ['Authorization' => 'Bearer ' . 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZGVmdnN4YW1uc2NjZWNjY3p1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MDAwOTgxNCwiZXhwIjoxOTg1NTg1ODE0fQ.kZKF_5HedaYHIi4aL77r2PJa5LGeyGlvVnL-tKstycc'];
+$authHeader = ['Authorization' => 'Bearer '.'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZGVmdnN4YW1uc2NjZWNjY3p1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MDAwOTgxNCwiZXhwIjoxOTg1NTg1ODE0fQ.kZKF_5HedaYHIi4aL77r2PJa5LGeyGlvVnL-tKstycc'];
 $bucket_id = 'test-bucket';
 $client = new  StorageFile(
-	'https://' . 'gpdefvsxamnscceccczu' . '.supabase.co/storage/v1',
+	'https://'.'gpdefvsxamnscceccczu'.'.supabase.co/storage/v1',
 	$authHeader,
 	$bucket_id
 );
@@ -38,14 +38,12 @@ function exampleUpdate(): void
 	print_r($result);
 }
 
-
 function exampleMove(): void
 {
 	global $client;
 	$result = $client->move('path/to/file.png', 'to/new-path/file.png');
 	print_r($result);
 }
-
 
 function exampleCopy(): void
 {
@@ -54,7 +52,6 @@ function exampleCopy(): void
 	print_r($result);
 }
 
-
 function exampleCreateSignedUrl(): void
 {
 	global $client;
@@ -62,7 +59,6 @@ function exampleCreateSignedUrl(): void
 	$result = $client->createSignedUrl('path/to/file.png', 60, $options);
 	print_r($result);
 }
-
 
 function exampleCreateSignedUrls(): void
 {
