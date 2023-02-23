50f15ee3d1cdf147bc35cd1a9bf3745991fb9c16
Norm <tehnorm@users.noreply.github.com>
2023-02-23 23:34:56 +0000

    Apply fixes from StyleCI

--- examples/handling-buckets/empty-bucket.php
+++ examples/handling-buckets/empty-bucket.php
@@ -2,6 +2,7 @@
 
 include dirname(__DIR__, 1).'\header.php';
 use Supabase\Storage\StorageClient;
+
 $authHeader = ['Authorization' => "Bearer {$api_key}"];
 $client = new  StorageClient(
 	"https://{$supabase_id}.supabase.co/storage/v1",
