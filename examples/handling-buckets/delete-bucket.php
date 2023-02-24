529a39fe73d9fe8176f6463df24d1c8e17bdbc46
WichinRighetti <lamrighetti@gmail.com>
2023-02-23 16:21:54 -0800

    fixed referencing on directories

--- examples/handling-buckets/delete-bucket.php
+++ examples/handling-buckets/delete-bucket.php
@@ -1,6 +1,6 @@
 <?php
 
-include dirname(__DIR__, 1).'\header.php';
+include dirname(__DIR__, 1).'/header.php';
 use Supabase\Storage\StorageClient;
 
 $authHeader = ['Authorization' => "Bearer {$api_key}"];
--- examples/header.php
+++ examples/header.php
@@ -1,6 +1,6 @@
 <?php
 
-include dirname(__DIR__, 1).'\vendor\autoload.php';
+include dirname(__DIR__, 1).'/vendor/autoload.php';
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createUnsafeImmutable(__DIR__);
