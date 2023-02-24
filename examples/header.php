26d16fd4772fbed82abcb075712bbe56a8b7350d
Norm <santnorm@gmail.com>
2023-02-23 19:19:29 -0700

    Use the current dir for the examples .env

--- examples/header.php
+++ examples/header.php
@@ -3,7 +3,7 @@
 include __DIR__.'/../vendor/autoload.php';
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createUnsafeImmutable(__DIR__.'/');
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
 $api_key = getenv('API_KEY');
