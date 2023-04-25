8c8c4ad617fa50d47f23debc1e09ae71071f611d
WichinRighetti <lamrighetti@gmail.com>
2023-04-25 14:09:58 -0700

    done with examples

--- examples/handling-files/update-file.php
+++ examples/handling-files/update-file.php
@@ -1,14 +1,21 @@
 <?php
 
-include __DIR__.'/../header.php';
+include __DIR__ . '/../header.php';
 
 use Supabase\Storage\StorageFile;
 
+//Selecting an already created bucket for our test.
 $bucket_id = 'test-bucket';
-
-$client = new StorageFile($api_key, $reference_id, $bucket_id);
-
-$options = ['upsert' => true];
-$result = $client->update('path/file.png', 'https://cdn-icons-png.flaticon.com/128/7267/7267612.png', $options);
+//Also creating file with unique ID.
+$testFile = 'file' . uniqid() . '.png';
+//Creating our StorageFile instance to upload files.
+$file = new StorageFile($api_key, $reference_id, $bucket_id);
+//We will upload a test file to update it.
+$file->upload($testFile, 'https://www.shorturl.at/img/shorturl-icon.png', ['public' => false]);
+//Now we will update the file using the update method.
+$result = $file->update($testFile, 'https://cdn-icons-png.flaticon.com/128/7267/7267612.png', ['upsert' => true]);
+//print out result.
 $output = json_decode($result->getBody(), true);
 print_r($output);
+//delete example files.
+$file->remove(["$testFile"]);
--- examples/handling-files/upload-file-from-base64.php
+++ examples/handling-files/upload-file-from-base64.php
@@ -1,17 +1,23 @@
 <?php
 
-include __DIR__.'/../header.php';
+include __DIR__ . '/../header.php';
+
 use Supabase\Storage\StorageFile;
 
+//Selecting an already created bucket for our test.
 $bucket_id = 'test-bucket';
-
-$client = new StorageFile($api_key, $reference_id, $bucket_id);
-
-$options = ['contentType' => 'image/png'];
-// Get the image and convert into string
+//Also creating file with unique ID.
+$testFile = 'file' . uniqid() . '.png';
+//Creating our StorageFile instance to upload files.
+$file = new StorageFile($api_key, $reference_id, $bucket_id);
+//We will upload a test file. And get the image and convert into string
 $img = file_get_contents(
-	'https://images.squarespace-cdn.com/content/v1/6351e8dab3ca291bb37a18fb/c097a247-cbdf-4e92-a5bf-6b52573df920/1666314646844.png');
-$data = base64_encode($img);
-$result = $client->upload('path/to/file-base64.png', $data, $options);
+	'https://images.squarespace-cdn.com/content/v1/6351e8dab3ca291bb37a18fb/c097a247-cbdf-4e92-a5bf-6b52573df920/1666314646844.png'
+);
+$data = $img;
+$result = $file->upload($testFile, $data, ['contentType' => 'image/png']);
+//print out result.
 $output = json_decode($result->getBody(), true);
 print_r($output);
+//delete example files.
+$file->remove(["$testFile"]);
