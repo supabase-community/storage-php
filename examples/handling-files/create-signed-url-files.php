cb53aa6594c97a9c13bb85832086e9398f331d38
Norm <tehnorm@users.noreply.github.com>
2023-04-25 20:43:00 +0000

    Apply fixes from StyleCI

--- examples/handling-files/create-signed-url-files.php
+++ examples/handling-files/create-signed-url-files.php
@@ -1,14 +1,14 @@
 <?php
 
-include __DIR__ . '/../header.php';
+include __DIR__.'/../header.php';
 
 use Supabase\Storage\StorageFile;
 
 //Selecting an already created bucket for our test.
 $bucket_id = 'test-bucket';
 //Also creating file with unique ID.
-$testFile = 'file' . uniqid() . '.png';
-$testFile2 = 'file' . uniqid() . '.png';
+$testFile = 'file'.uniqid().'.png';
+$testFile2 = 'file'.uniqid().'.png';
 //Creating our StorageFile instance to upload files.
 $file = new StorageFile($api_key, $reference_id, $bucket_id);
 //We will upload a test file to retrieve the URL.
