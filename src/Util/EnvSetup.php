55f26b964ecf47034bf0c85b8df792f86df35d9c
Norm Santos <santnorm@gmail.com>
2023-04-24 23:23:36 -0600

    Fixing typo, again.

--- src/Util/EnvSetup.php
+++ src/Util/EnvSetup.php
@@ -25,11 +25,11 @@ class EnvSetup
 		}
 
 		if (empty($apiKey)) {
-			throw new \Exception('Could not loaded API_KEY');
+			throw new \Exception('Could not load API_KEY');
 		}
 
 		if (empty($refId)) {
-			throw new \Exception('Could not loaded API_KEY');
+			throw new \Exception('Could not load REFERENCE_ID');
 		}
 
 		return [
