2e4924e4dcf0f24025b162da9defb852e0bc857a
WichinRighetti <lamrighetti@gmail.com>
2023-04-25 13:42:51 -0700

    list, remove, move, and download done

--- examples/handling-files/move-file.php
+++ examples/handling-files/move-file.php
@@ -1,12 +1,21 @@
 <?php
 
-include __DIR__.'/../header.php';
+include __DIR__ . '/../header.php';
 
 use Supabase\Storage\StorageFile;
 
+//Selecting an already created bucket for our test.
 $bucket_id = 'test-bucket';
-
-$client = new StorageFile($api_key, $reference_id, $bucket_id);
-$result = $client->move('test-bucket', 'to/new-path/test-bucket');
+//Also creating file with unique ID.
+$testFile = 'file' . uniqid() . '.png';
+//Creating our StorageFile instance to upload files.
+$file = new StorageFile($api_key, $reference_id, $bucket_id);
+//We will upload a test file to move it.
+$file->upload($testFile, 'https://www.shorturl.at/img/shorturl-icon.png', ['public' => false]);
+//Now we will relocate the file using the move method.
+$result = $file->move("$testFile", "path/$testFile");
+//print out result.
 $output = json_decode($result->getBody(), true);
 print_r($output);
+//delete example files.
+$file->remove(["path/$testFile"]);
